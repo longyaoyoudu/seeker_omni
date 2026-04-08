b648ff42c0f6107a21cec3ce93296ef3767d636b
bryant <126932731+longyaoyoudu@users.noreply.github.com>
2026-04-08 15:48:00 +0800

    Add files via upload

---
+++ dataprep/prepare/__main__.py
@@ -0,0 +1,13 @@
+#from . import run
+#from ..data_paths import default_dataprep_cfg
+
+#if __name__ == "__main__":
+#    cfg = default_dataprep_cfg()
+#    run(cfg)
+
+from dataprep import run_prepare
+
+if __name__ == "__main__":
+    run_prepare()
+
+
